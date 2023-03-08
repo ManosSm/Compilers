48ae48cd0353a45a4ed00c38eb74679c8ece7d44
Ergo-42 <92264086+Ergo-42@users.noreply.github.com>
2023-03-08 15:34:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import string
 
 
 class Token:
-    family = ""
-    recognized_string = ""
-    line_number = 0
 
     def __init__(self, family, recognized_string, line_number):
         self.family = family
@@ -13,17 +10,62 @@ class Token:
 
 
 class Lex:
-    current_Line = 0
-    file_name = ""
-    token = None
-
+    
     def __init__(self, file_name):
         self.file_name = file_name
+        self.file = open(self.file_name)
+        self.token = None
+        self.current_Line = 0
+        self.symbol_dict=   {
+                                "_":2,#
+                                "+":3,#
+                                "-":4,#
+                                "*":5,#
+                                "/":6,#
+                                "<":7,#
+                                ">":8,#
+                                "!":9,#
+                                "=":10,#
+                                ";":11,#
+                                ",":12,#
+                                ":":13,#
+                                "[":14,#
+                                "]":15,#
+                                "(":16,#
+                                ")":17,#
+                                "#":18,#
+                                "{":19,#
+                                "}":20,#
+                                "$":21#
+                            }
+
+                            #0 are letters   1 are digits    22 are blank chars    23 eof   99 error
+        self.state_list=[   [3,1,99,5,5,6,7,16,17,18,13,12,12,12,8,8,8,8,9,99,99,99,0,99],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            [],
+                            []
+                        ]
 
     def next_token(self):
-        file = open(self.file_name)
+        
         while 1:
-            token = file.read(1)
+            token = self.file.read(1)
 
             if not token:
                 break
