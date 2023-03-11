4aa6e57ae95e08723709cc81985457a23cc23225
ManosSm <92254214+ManosSm@users.noreply.github.com>
2023-03-11 13:44:23 +0200

    Basic state management

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ class Token:
 
 
 class Lex:
-    
+
     def __init__(self, file_name):
         self.file_name = file_name
-        self.file = open(self.file_name)
         self.token = None
-        self.current_Line = 0
+        self.current_line = 0
+        self.current_char = 0
+        self.state = 0
         self.symbol_dict=   {
                                 "_":2,#
                                 "+":3,#
@@ -42,111 +43,68 @@ class Lex:
                                 "$":21#
                             }
 
-                            #0 are letters   1 are digits    22 are blank chars    23 eof   99 error
-        self.state_list=[   [3,1,99,5,5,6,7,16,17,18,13,12,12,12,8,8,8,8,9,99,99,99,0,99],#0
+                            #0 are letters   1 are digits    22 blank char except eof    23 eof   99 error
+        self.state_list = [
+                            [3,1,99,5,5,6,7,17,18,19,13,12,12,12,8,8,8,8,9,99,99,99,0,20],#0
                             [2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],#1
-                            [-----------------------------------------------],#2
+                            [-1],#2 (-1 is the current_char offset)
                             [3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],#3
-                            [-----------------------------------------------],#4
-                            [-----------------------------------------------],#5
-                            [-----------------------------------------------],#6
+                            [-1],#4
+                            [0],#5
+                            [0],#6
                             [99,99,99,99,99,99,6,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99],#7
-                            [-----------------------------------------------],#8
+                            [0],#8
                             [99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,8,8,10,99,99],#9
                             [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11,10,10,10,10,99],#10
                             [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,0,10,99],#11
-                            [-----------------------------------------------],#12
+                            [0],#12
                             [14,14,14,14,14,14,14,14,14,14,15,14,14,14,14,14,14,14,14,14,14,14,14,14],#13
-                            [-----------------------------------------------],#14
-                            [-----------------------------------------------],#15
-                            [15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],#16
-                            [15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],#17
-                            [15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],#18
-                            [?]#19
+                            [-1],#14
+                            [0],#15
+                            [-1],#16
+                            [16,16,16,16,16,16,16,16,16,16,15,16,16,16,16,16,16,16,16,16,16,16,16,16],#17
+                            [16,16,16,16,16,16,16,16,16,16,15,16,16,16,16,16,16,16,16,16,16,16,16,16],#18
+                            [99,99,99,99,99,99,99,99,99,99,15,99,99,99,99,99,99,99,99,99,99,99,99,99],#19
+                            [0]#20
                         ]
 
     def next_token(self):
-        
+        file = open(self.file_name)
+        file.seek(self.current_char)
+
         while 1:
-            token = self.file.read(1)
+            in_read = file.read(1)
+            in_id = 99
 
-            if not token:
-                break
-            print(token in string.whitespace)
+            if in_read.isalpha():  # if letter
+                in_id = 0
+            elif in_read.isnumeric():  # if number
+                in_id = 1
+            elif in_read in string.whitespace:  # if a white character
+                if in_read == "\n":           # if changed line
+                    self.current_line += 1
+
+                print("current line:", self.current_line)
+
+                if in_read == "":     # if eof
+                    in_id = 23
+                else:
+                    in_id = 22        # if white character except eof
+            elif in_read in self.symbol_dict:
+                in_id = self.symbol_dict[in_read]
+            else:
+                print("Error: unknown character", in_read, "at line", self.current_line)
+                exit(1)
+
+            self.state = self.state_list[self.state][in_id]
+
+            print(in_read)
+            print(self.state)
 
 
-if __name__ == '__main__':
-    lex = Lex("test.txt")
-    lex.next_token()
 
-
-    def __init__(self, family, recognized_string, line_number):
-        self.family = family
-        self.recognized_string = recognized_string
-        self.line_number = line_number
-
-
-class Lex:
-    
-    def __init__(self, file_name):
-        self.file_name = file_name
-        self.file = open(self.file_name)
-        self.token = None
-        self.current_Line = 0
-        self.symbol_dict=   {
-                                "_":2,#
-                                "+":3,#
-                                "-":4,#
-                                "*":5,#
-                                "/":6,#
-                                "<":7,#
-                                ">":8,#
-                                "!":9,#
-                                "=":10,#
-                                ";":11,#
-                                ",":12,#
-                                ":":13,#
-                                "[":14,#
-                                "]":15,#
-                                "(":16,#
-                                ")":17,#
-                                "#":18,#
-                                "{":19,#
-                                "}":20,#
-                                "$":21#
-                            }
-
-                            #0 are letters   1 are digits    22 are blank chars    23 eof   99 error
-        self.state_list=[   [3,1,99,5,5,6,7,16,17,18,13,12,12,12,8,8,8,8,9,99,99,99,0,99],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            [],
-                            []
-                        ]
-
-    def next_token(self):
-        
-        while 1:
-            token = self.file.read(1)
-
-            if not token:
-                break
-            print(token in string.whitespace)
+        self.current_char = file.tell()
+        file.close()
 
 
 if __name__ == '__main__':
