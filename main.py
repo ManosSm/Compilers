cefffd428785aac5f6c2a25c88de721e874e3599
Ergo-42 <92264086+Ergo-42@users.noreply.github.com>
2023-03-12 16:56:12 +0200

    update

--- main.py
+++ main.py
@@ -16,10 +16,21 @@ class Lex:
 
     def __init__(self, file_name):
         self.file_name = file_name
-        self.token = None
         self.current_line = 0
         self.current_char = 0
-        self.state = 0
+        self.keyword_set={"def","not","and","or","declare","print","return","if","else","while"}                   #TODO
+        self.final_state_set=   {  
+                                    2:"number",
+                                    4:"id/keyword",         #==================HERE+++++++======                   #TODO
+                                    5:"addOparator",
+                                    6:"mulOp",
+                                    8:"groupSymbol",
+                                    12:"delimitor",
+                                    14:"assignment",
+                                    15:"relOperator",
+                                    16:"relOperator",
+                                    20:"EOF"
+                                }
         self.symbol_dict=   {
                                 "_":2,#
                                 "+":3,#
@@ -42,6 +53,8 @@ class Lex:
                                 "}":20,#
                                 "$":21#
                             }
+
+        
 
                             #0 are letters   1 are digits    22 blank char except eof    23 eof   99 error
         self.state_list = [
@@ -71,42 +84,61 @@ class Lex:
     def next_token(self):
         file = open(self.file_name)
         file.seek(self.current_char)
+        state=0
+        rec_string=""
 
         while 1:
             in_read = file.read(1)
             in_id = 99
 
-            if in_read.isalpha():  # if letter
+            if in_read.isalpha():                           # if letter
                 in_id = 0
-            elif in_read.isnumeric():  # if number
+                rec_string+=in_read                         # adding character to the recognised string
+            elif in_read.isnumeric():                       # if number
                 in_id = 1
-            elif in_read in string.whitespace:  # if a white character
-                if in_read == "\n":           # if changed line
+                rec_string+=in_read                         # adding character to the recognised string
+            elif in_read in string.whitespace:              # if a white character
+                if in_read == "\n":                         # if changed line
                     self.current_line += 1
 
                 print("current line:", self.current_line)
 
-                if in_read == "":     # if eof
+                if in_read == "":                           # if eof
                     in_id = 23
                 else:
-                    in_id = 22        # if white character except eof
+                    in_id = 22                              # if white character except eof
             elif in_read in self.symbol_dict:
                 in_id = self.symbol_dict[in_read]
+                rec_string+=in_read                         # adding character to the recognised string
             else:
                 print("Error: unknown character", in_read, "at line", self.current_line)
+                file.close()
                 exit(1)
 
-            self.state = self.state_list[self.state][in_id]
-
+            state = self.state_list[state][in_id]                        
+            
             print(in_read)
-            print(self.state)
+            print(state)
+            
+            if state==0:                                        #NOTE: maybe improveivanb;ele
+                rec_string=""
+            
+            if state in self.final_state_set:                   # if state is final
+                rec_string = rec_string[0:(len(rec_string) + self.state_list[state][0])] #black box 
+                self.current_char = file.tell() + self.state_list[state][0]
+                file.close()
+                return(Token(self.final_state_set[state], rec_string, self.current_line))
+           
 
 
 
-        self.current_char = file.tell()
-        file.close()
+        
 
 
 if __name__ == '__main__':
     lex = Lex("test.txt")
-    lex.next_token()
+
+    for i in range(10):
+
+        tnk=lex.next_token()
+        print("token:",tnk.family,tnk.recognized_string,tnk.line_number)
